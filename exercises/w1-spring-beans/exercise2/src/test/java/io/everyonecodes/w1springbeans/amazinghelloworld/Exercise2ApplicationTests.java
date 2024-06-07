90034d61012d31d079ea81afef1331dd41b4a207
crv-9 <159793135+crv-9@users.noreply.github.com>
2024-06-07 11:36:22 +0200

    Add week1-exercise1

---
+++ exercises/w1-spring-beans/exercise2/src/test/java/io/everyonecodes/w1springbeans/amazinghelloworld/Exercise2ApplicationTests.java
@@ -0,0 +1,13 @@
+package io.everyonecodes.w1springbeans.amazinghelloworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Exercise2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
